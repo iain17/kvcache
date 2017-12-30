93daff7b510225561d62ddc0c5bb01b29e56bd32
Iain Munro <iain17@me.com>
2017-12-30 18:04:15 +0100

    concurrent read/write panic fixed. Removed defer, adding rwlock: Optimizing performance.

--- lttlru/lttlru.go
+++ lttlru/lttlru.go
@@ -13,7 +13,7 @@ import (
 type LruWithTTL struct {
 	*hlru.Cache
 	expiresAt map[interface{}]*time.Time
-	expireMutex sync.Mutex
+	expireMutex sync.RWMutex
 }
 
 // NewTTL creates an LRU of the given size
@@ -30,37 +30,49 @@ func NewTTLWithEvict(size int, onEvicted func(key interface{}, value interface{}
 	if err != nil {
 		return nil, err
 	}
-	return &LruWithTTL{c, make(map[interface{}]*time.Time), sync.Mutex{}}, nil
+	return &LruWithTTL{c, make(map[interface{}]*time.Time), sync.RWMutex{}}, nil
 }
 
 func (lru *LruWithTTL) removeExpired(key interface{}) {
 	lru.expireMutex.Lock()
-	defer lru.expireMutex.Unlock()
 	delete(lru.expiresAt, key)
 	lru.Remove(key)
+	lru.expireMutex.Unlock()
 }
 
 // AddWithTTL add an key:val with TTL
 func (lru *LruWithTTL) AddWithTTL(key, value interface{}, ttl time.Duration) bool {
 	lru.expireMutex.Lock()
-	defer lru.expireMutex.Unlock()
 	expire := time.Now().Add(ttl)
 	lru.expiresAt[key] = &expire
+	lru.expireMutex.Unlock()
+
 	return lru.Cache.Add(key, value)
 }
 
-func (lru *LruWithTTL) GetWithTTL(key interface{}) (interface{}, bool) {
+func (lru *LruWithTTL) Contains(key interface{}) bool {
+	lru.expireMutex.RLock()
 	if lru.expiresAt[key] != nil {
-		if lru.expiresAt[key].After(time.Now()) {
-			// ttl not expired
-			return lru.Get(key)
+		if !lru.expiresAt[key].After(time.Now()) {
+			lru.removeExpired(key)
+			lru.expireMutex.Unlock()
+			return false
 		}
-
-		// ttl expired
-		lru.removeExpired(key)
 	}
+	lru.expireMutex.Unlock()
+	return lru.Cache.Contains(key)
+}
 
-	// not added with ttl
-	return nil, false
+func (lru *LruWithTTL) Get(key interface{}) (interface{}, bool) {
+	lru.expireMutex.RLock()
+	if lru.expiresAt[key] != nil {
+		if !lru.expiresAt[key].After(time.Now()) {
+			lru.removeExpired(key)
+			lru.expireMutex.Unlock()
+			return nil, false
+		}
+	}
+	lru.expireMutex.Unlock()
+	return lru.Cache.Get(key)
 }
 
